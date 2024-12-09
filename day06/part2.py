8bda0dfd3c989c192ffae8c5044bf1be6691b565
Maarten Smeyers <maartensmeyers@gmail.com>
2024-12-09 22:23:24 +0100

    Day 6 optimization - work in progress

--- day06/part2.py
+++ day06/part2.py
@@ -1,5 +1,5 @@
 def process_input():
-    with open("input.txt") as f:
+    with open("test1.txt") as f:
         lines = f.read().splitlines()
         grid = [[x for x in y] for y in lines]
         return grid
@@ -21,62 +21,106 @@ def find_char(grid, char):
     return -1, -1
 
 
+def step(grid, guard, x, y, off_the_grid):
+    off_the_grid = False
+    if guard == '^':
+        if y == 0:
+            off_the_grid = True
+        if grid[y - 1][x] == '#':
+            guard = '>'
+            if x == len(grid[0]) - 1:
+                off_the_grid = True
+        else:
+            y -= 1
+    elif guard == '>':
+        if x == len(grid[0]) - 1:
+            off_the_grid = True
+        if grid[y][x + 1] == '#':
+            guard = 'V'
+            if y == len(grid) - 1:
+                off_the_grid = True
+        else:
+            x += 1
+    elif guard == 'V':
+        if y == len(grid) - 1:
+            off_the_grid = True
+        if grid[y + 1][x] == '#':
+            guard = '<'
+            if x == 0:
+                off_the_grid = True
+        else:
+            y += 1
+    elif guard == '<':
+        if x == 0:
+            off_the_grid = True
+        if grid[y][x - 1] == '#':
+            guard = '^'
+            if y == 0:
+                off_the_grid = True
+        else:
+            x -= 1
+
+    return grid, guard, x, y, off_the_grid
+
+
+def look_ahead(grid, guard, x, y):
+    off_the_grid = False
+    char = ''
+    if guard == '^':
+        if y == 0:
+            off_the_grid = True
+        else:
+            char = grid[y - 1][x]
+    elif guard == '>':
+        if x == len(grid[0]) - 1:
+            off_the_grid = True
+        else:
+            char = grid[y][x + 1]
+    elif guard == 'V':
+        if y == len(grid) - 1:
+            off_the_grid = True
+        else:
+            char = grid[y + 1][x]
+    elif guard == '<':
+        if x == 0:
+            off_the_grid = True
+        else:
+            char = grid[y][x - 1]
+
+    return off_the_grid, char
+
+
 def solve():
     start_grid = process_input()
     guard = '^'
-    obstr = 'O'
+    # obstr = 'O'
     x, y = find_char(start_grid, guard)
     start_x = x
     start_y = y
     start_guard = guard
     total = 0
-    for obstr_y in range(len(start_grid)):
-        for obstr_x in range(len(start_grid[0])):
-            if obstr_x == start_x and obstr_y == start_y:
-                continue
-            print(obstr_x, obstr_y)
-            guard = start_guard
-            x = start_x
-            y = start_y
-            grid = [x[:] for x in start_grid]
-            grid[obstr_y][obstr_x] = obstr
-            been_there = []
-            while True:
-                been_there.append((guard, x, y))
-                grid[y][x] = 'X'
-                if guard == '^':
-                    if y == 0:
-                        break
-                    if grid[y - 1][x] == '#' or grid[y - 1][x] == 'O':
-                        guard = '>'
-                    else:
-                        y -= 1
-                elif guard == '>':
-                    if x == len(grid[0]) - 1:
-                        break
-                    if grid[y][x + 1] == '#' or grid[y][x + 1] == 'O':
-                        guard = 'V'
-                    else:
-                        x += 1
-                elif guard == 'V':
-                    if y == len(grid) - 1:
-                        break
-                    if grid[y + 1][x] == '#' or grid[y + 1][x] == 'O':
-                        guard = '<'
-                    else:
-                        y += 1
-                elif guard == '<':
-                    if x == 0:
-                        break
-                    if grid[y][x - 1] == '#' or grid[y][x - 1] == 'O':
-                        guard = '^'
-                    else:
-                        x -= 1
+    max_steps = 1
+    while True:
+        grid = [x[:] for x in start_grid]
+        guard = start_guard
+        x = start_x
+        y = start_y
+        off_the_grid = False
+        for i in range(max_steps):
+            grid, guard, x, y, off_the_grid = step(grid, guard, x, y, off_the_grid)
+        if off_the_grid is True:
+            break
+        # grid[obstr_y][obstr_x] = obstr
+        been_there = []
+        while True:
+            been_there.append((guard, x, y))
+            step(grid, guard, x, y, off_the_grid)
 
-                if (guard, x, y) in been_there:
-                    # we are in a loop
-                    total += 1
-                    break
+            if (guard, x, y) in been_there:
+                # we are in a loop
+                total += 1
+                break
+
     print(total)
 
 
